d4aed5981b5bae8c7df91c77569efdace5c03a7f
Alexey Maksutov <alexey.maksutov@gmail.com>
2026-05-04 02:50:08 -0400

    updates

--- etc/run.d/99-beanstalkd.sh
+++ etc/run.d/99-beanstalkd.sh
@@ -1,10 +1,8 @@
-#!/bin/sh
-
-exec beanstalkd \
+suexec beanstalkd \
 	-b /var/lib/beanstalkd \
 	-u "$PUSER" \
 	$(test -n "$FSYNC_MS" && echo -f $FSYNC_MS) \
 	$(test -n "$MAX_JOB_SIZE" && echo -z $MAX_JOB_SIZE) \
 	$(test -n "$WAL_FILE_SIZE" && echo -s $WAL_FILE_SIZE) \
 	${DONT_COMPACT_BINLOG:+-n} \
-	${VERBOSE:+-V}
+	${VERBOSE:+-V} &
